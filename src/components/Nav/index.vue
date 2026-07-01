a1ae3356a89c2d151195a29384577b58b86edb3d
XunQiu123 <2704588127@qq.com>
2026-07-01 22:08:56 +0800

    ⚙️ feat: 新增导航栏组件并调整全局样式
    - 新建导航栏组件，包含标题、链接区和功能区
    - 为页面根元素添加宽度适配和最小宽度限制
    - 实现导航标题点击切换显示文本功能

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -1,6 +1,16 @@
 <template>
-    
+    <div class="flex nav">
+        <div class="flex nav_title">
+            <span class="nav_title_icon"></span>
+            <span @click="title = !title" class="nav_title_text">{{ title? "不知名の小站" : "Made by XunQiu" }}</span>
+        </div>
+        <div class="nav_links"></div>
+        <div class="nav_func"></div>
+    </div>
 </template>
 <script setup lang="ts" Name="Nav">
+    import { ref } from 'vue';
     import './index.css'
+
+    let title = ref(true)
 </script>
