050eb88c4b6ea0f47b8484b3d54eead235801b3a
XunQiu123 <2704588127@qq.com>
2026-07-01 20:06:05 +0800

    ⚙️ feat: 初始化导航栏并添加全局字体与背景样式

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -3,9 +3,13 @@
         <div class="flex nav">
             <div class="flex nav_title">
                 <span class="nav_title_icon"></span>
+                <span class="nav_title_text">不知名の小站</span>
             </div>
             <div class="flex nav_links">
 
+            </div>
+            <div class="nav_func">
+                
             </div>
         </div>
     </div>
