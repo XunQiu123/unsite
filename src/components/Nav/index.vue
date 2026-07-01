02f0d24d4fe39a6ade191b29704d5d2e25b3c515
XunQiu123 <2704588127@qq.com>
2026-07-01 20:20:05 +0800

    🎲 style: 调整导航栏、全局样式与导航交互
    1. 给导航标题添加点击切换标题功能
    2. 新增全局禁止选中文本样式
    3. 修正背景图片路径
    4. 给button初始化默认样式
    5. 给导航标题添加指针光标

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -3,17 +3,28 @@
         <div class="flex nav">
             <div class="flex nav_title">
                 <span class="nav_title_icon"></span>
-                <span class="nav_title_text">不知名の小站</span>
+                <span @click="changeTitle" class="nav_title_text">{{ title }}</span>
             </div>
             <div class="flex nav_links">
 
             </div>
             <div class="nav_func">
-                
+
             </div>
         </div>
     </div>
 </template>
 <script setup lang="ts" Name="Nav">
+    import { ref } from 'vue';
     import './index.css'
+
+    let title = ref<string>("不知名の小站")
+
+    function changeTitle(){
+        if (title.value == "不知名の小站"){
+            title.value = "Made by XunQiu"
+        }else{
+            title.value = "不知名の小站"
+        }
+    }
 </script>
