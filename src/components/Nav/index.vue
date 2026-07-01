9257590d430c4d263f43b3b4e59feba04d4cb6bd
XunQiu123 <2704588127@qq.com>
2026-07-01 19:48:04 +0800

    feat: 初始化导航组件并添加全局边框变量
    1. 新增全局CSS变量--unsite-border用于统一边框样式
    2. 新增导航组件基础结构与样式
    3. 添加网站导航栏图标并配置相关样式

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="flex nav_main">
         <div class="flex nav">
-            
+            <div class="flex nav_title">
+                <span class="nav_title_icon"></span>
+            </div>
+            <div class="flex nav_links">
+
+            </div>
         </div>
     </div>
 </template>
