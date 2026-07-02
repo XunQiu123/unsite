05e6ed50942f97b1c57e05bbd70595e6b808c60d
XunQiu123 <2704588127@qq.com>
2026-07-02 15:14:12 +0800

    🧱 refactor: 修复导航链接循环变量命名不一致问题

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -6,7 +6,7 @@
                 <span @click="title = !title" class="nav_title_text">{{ title? "不知名の小站" : "Made by XunQiu" }}</span>
             </div>
             <div class="flex nav_links">
-                <span class="nav_links_text" v-for="value in links" :key="value.id">{{ value.text }}</span>
+                <span class="nav_links_text" v-for="link in links" :key="link.id">{{ link.text }}</span>
             </div>
             <div class="nav_func"></div>
         </div>
