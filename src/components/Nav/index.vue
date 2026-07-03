97886fa7a404cde829071eebd550075d1a973c6b
XunQiu123 <2704588127@qq.com>
2026-07-03 21:39:43 +0800

    ⚙️ feat: 引入iconfont替换原有导航svg图标
    1. 新增iconfont.css并在入口文件引入
    2. 替换导航栏原svg图标为iconfont图标
    3. 全局css中移除旧的iconfont定义，新增图标背景样式
    4. 删除不再使用的nav_icon.svg文件

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -2,14 +2,14 @@
     <div class="flex nav_main">
         <div class="flex nav">
             <div class="flex nav_title">
-                <span class="nav_title_icon"></span>
+                <i class="iconfont icon-bg" style="font-size: 19px;">&#xe600;</i>
                 <span @click="title = !title" class="nav_title_text" :data-text="title ? '不知名の小站' : 'Made by XunQiu'">{{ title? "不知名の小站" : "Made by XunQiu" }}</span>
             </div>
             <div class="flex nav_links">
                 <span class="nav_links_text" v-for="link in links" :key="link.id">{{ link.text }}</span>
             </div>
             <div class="flex nav_func">
-                <i class="iconfont">&#xe885;</i>
+                <i class="iconfont icon-bg">&#xe885;</i>
             </div>
         </div>
     </div>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import '../public/css/global.css'
+import '../public/css/iconfont.css'
 import App from './App.vue'
 
 const app =  createApp(App)
