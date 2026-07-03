97df8f496198dced8ac71a685b3cd244f8690f94
XunQiu123 <2704588127@qq.com>
2026-07-03 22:41:08 +0800

    🔧 fix: 更新导航栏标题切换的显示文本

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -3,7 +3,7 @@
         <div class="flex nav">
             <div class="flex nav_title">
                 <i class="iconfont icon-bg" style="font-size: 19px;">&#xe600;</i>
-                <span @click="title = !title" class="nav_title_text" :data-text="title ? '不知名の小站' : 'Made by XunQiu'">{{ title? "不知名の小站" : "Made by XunQiu" }}</span>
+                <span @click="title = !title" class="nav_title_text" :data-text="title ? '不知名の小站' : 'Made by XunQiu & YZR'">{{ title? "不知名の小站" : "Made by XunQiu" }}</span>
             </div>
             <div class="flex nav_links">
                 <span class="nav_links_text" v-for="link in links" :key="link.id">{{ link.text }}</span>
@@ -21,6 +21,6 @@
     import links from '../../assets/json/nav_links.json'
 
     //true "不知名の小站"
-    //false "Made by XunQiu"
+    //false "Made by XunQiu & YZR"
     let title = ref<boolean>(true)
 </script>
