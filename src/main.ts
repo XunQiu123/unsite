ed646d7888173b41a27a50c8bae155899c22967b
XunQiu123 <2704588127@qq.com>
2026-07-03 21:50:38 +0800

    📦 build: 调整静态资源文件路径，迁移资源到项目指定目录

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import '../public/css/global.css'
-import '../public/css/iconfont.css'
+import './assets/css/global.css'
+import './assets/css/iconfont.css'
 import App from './App.vue'
 
 const app =  createApp(App)
