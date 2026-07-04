2275bd99ca6164e7e12ccf0deea47d4a168ba726
XunQiu123 <2704588127@qq.com>
2026-07-04 13:42:01 +0800

    🧱 refactor: 添加Readme组件并调整导航栏样式

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
     <div>
         <Nav/>
         <Welcome/>
+        <Readme/>
     </div>
     
 </template>
 <script setup lang="ts" Name="App">
     import Nav from './components/Nav/index.vue'
     import Welcome from './components/Welcome/index.vue'
+    import Readme from './components/Readme/index.vue'
 </script>
--- src/components/Readme/index.vue
+++ src/components/Readme/index.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="readme_main">
+    <div class="flex readme_main">
         <div class="readme">
-            
+            test
         </div>
     </div>
 </template>
 <script setup lang="ts" name="Readme">
-
+    import './index.css'
 </script>
