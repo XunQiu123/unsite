6e1c5df508afb4650706b256a17a3a0f86852750
XunQiu123 <2704588127@qq.com>
2026-07-23 22:07:07 +0800

    📃 docs: 更新README组件与全局样式
    1. 更新iconfont字体资源链接
    2. 重构README组件，添加开源声明与站点说明
    3. 新增全局a标签与code标签基础样式
    4. 补充全局CSS变量

--- src/components/Readme/index.vue
+++ src/components/Readme/index.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="flex readme_main">
         <div class="readme">
-            test
+            <i class="iconfont" style="margin-right: 20px;">&#xe88d;</i>
+            <span class="readme_text">本网站以</span>
+            <a href="https://mit-license.org/">MIT</a>
+            <span class="readme_text">协议开源。本站大量模仿、借鉴</span>
+            <a href="https://blog.imsyy.top/">無名小栈</a>
+            <span class="readme_text">与</span>
+            <a href="https://blog.zhheo.com/">张洪Heo</a>
+            <span class="readme_text">。目前正在开发中，之后可能会成为我的游戏官网、工作室/团队的子网站</span>
         </div>
     </div>
 </template>
